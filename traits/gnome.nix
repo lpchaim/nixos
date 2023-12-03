c4e0dfc34b71bf26dc54099384f9caef7e4e1545
Luna Perroni <lpchaim@proton.me>
2023-12-03 17:48:36 -0300

    feat: Add GNOME Photos to GNOME trait

--- traits/gnome.nix
+++ traits/gnome.nix
@@ -1,9 +1,15 @@
 # Use the GNOME desktop environment
 
-{ ... }:
+{ pkgs
+, ...
+}:
 
 {
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    gnome-photos
+  ];
 }
