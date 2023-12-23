e929ccb3a5e6ad1de3e7d2f515caf68eb6bac693
Luna Perroni <lpchaim@proton.me>
2023-12-23 20:23:10 -0300

    feat: auto-cpufreq tweaks

--- traits/laptop.nix
+++ traits/laptop.nix
@@ -6,5 +6,12 @@
   hardware.sensor.iio.enable = true;
   services.thermald.enable = true;
   # services.tlp.enable = true;
-  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        turbo = "never";
+      };
+    };
+  };
 }
