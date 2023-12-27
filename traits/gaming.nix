fcd61028ecb49f265c8dfddb84efa0e4120e6df5
Luna Perroni <lpchaim@proton.me>
2023-12-27 20:24:47 -0300

    chore: Simplify gaming trait

--- traits/gaming.nix
+++ traits/gaming.nix
@@ -1,10 +1,7 @@
-{ pkgs, ... }:
-
 {
   hardware.steam-hardware.enable = true;
   programs.steam = {
     enable = true;
-    package = pkgs.steam;
     gamescopeSession.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
