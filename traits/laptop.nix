91a9a2c803e15dfc7f6d3e531c895affa9e395f9
Luna Perroni <lpchaim@proton.me>
2023-12-27 10:14:16 -0300

    chore: Refactor laptop trait

--- traits/laptop.nix
+++ traits/laptop.nix
@@ -4,14 +4,16 @@
 
 {
   hardware.sensor.iio.enable = true;
-  services.thermald.enable = true;
-  # services.tlp.enable = true;
-  services.auto-cpufreq = {
-    enable = true;
-    settings = {
-      battery = {
-        turbo = "never";
+
+  services = {
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          turbo = "never";
+        };
       };
     };
+    thermald.enable = true;
   };
 }
