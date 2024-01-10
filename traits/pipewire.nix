cfcaf9ff009d5e433d7c858b028ffc1d7d834549
Luna Perroni <lpchaim@proton.me>
2024-01-10 19:07:17 -0300

    feat: Pipewire tweaks

--- traits/pipewire.nix
+++ traits/pipewire.nix
@@ -7,9 +7,11 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.enable = true;
   };
 }
