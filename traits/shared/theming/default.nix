7e4e400c2a38b23338d6425dbc11f1e9d6d4f12a
Luna Perroni <lpchaim@proton.me>
2024-06-03 21:13:10 -0300

    fix: Default theming exclusions
    Exclude gnome, gtk, kde and hyprland stylix customizations from non-nixos
    home configuration by default since those can softlock traditional
    installs.
    
    Disable plymouth stylix customizations on nixos systems by default since
    the vanilla theme looks vastly cleaner.

--- traits/shared/theming/default.nix
+++ traits/shared/theming/default.nix
@@ -1,13 +1,15 @@
 { lib, pkgs, ... }:
 
+with lib;
 {
   stylix = {
-    image = lib.mkDefault ../../../assets/wallpaper-city-d.png;
-    polarity = lib.mkDefault "dark";
-    base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    fonts.monospace = {
-      name = "JetBrainsMono";
-      package = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; });
+    autoEnable = mkDefault true;
+    image = mkDefault ../../../assets/wallpaper-city-d.png;
+    polarity = mkDefault "dark";
+    base16Scheme = mkDefault "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    fonts.monospace = mkDefault {
+      name = mkDefault "JetBrainsMono";
+      package = mkDefault (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; });
     };
   };
 }
