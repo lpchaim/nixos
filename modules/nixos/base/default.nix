aedadeec89ebbe7bcefa2cbe51e2a066b4e6b507
Lucas Chaim <lpchaim@gmail.com>
2024-06-20 13:07:02 -0300

    feat: Add wifi network priority

--- modules/nixos/base/default.nix
+++ modules/nixos/base/default.nix
@@ -63,12 +63,23 @@ in
   };
 
   # Networking
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [
-      57621 # spotify local discovery
-      5353 # spotify cast discovery
-    ];
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        57621 # spotify local discovery
+        5353 # spotify cast discovery
+      ];
+    };
+    dhcpcd.extraConfig =
+      let wifiOffset = 2000;
+      in ''
+        ssid Lpchaim5G
+        metric ${toString (wifiOffset - 20)}
+
+        ssid Lpchaim
+        metric ${toString (wifiOffset - 10)}
+      '';
   };
 
   # Internationalization
