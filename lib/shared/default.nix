38eff9b868ecf7c0b5b06088a8f45bc666ff588b
Luna Perroni <lpchaim@proton.me>
2024-07-02 13:05:47 -0300

    feat: Move binary cache configs to new shared lib

---
+++ lib/shared/default.nix
@@ -0,0 +1,17 @@
+{
+  shared = {
+    nix.settings = {
+      keep-outputs = true;
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://nyx.chaotic.cx"
+        "https://snowflakeos.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
+        "snowflakeos.cachix.org-1:gXb32BL86r9bw1kBiw9AJuIkqN49xBvPd1ZW8YlqO70="
+      ];
+    };
+  };
+}
