6ed731970c6347a87d5e4306247150553080f2c2
Lucas Chaim <lpchaim@gmail.com>
2024-08-04 14:50:05 -0300

    feat: Add nix-gaming
    Gaming tweaks, osu and some steam tweaks

--- modules/nixos/traits/gaming.nix
+++ modules/nixos/traits/gaming.nix
@@ -4,11 +4,26 @@
   hardware.steam-hardware.enable = true;
   programs.steam = {
     enable = true;
+    extraCompatPackages =
+      let
+        geVersions = [ "9-10" ];
+        protonGePackages = map
+          (v: pkgs.proton-ge-bin.overrideAttrs
+            (final: prev: { pname = prev.pname + v; version = "GE-Proton${v}"; }))
+          geVersions;
+      in
+      protonGePackages;
     gamescopeSession.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
+    platformOptimizations.enable = true;
+    protontricks.enable = true;
   };
   environment.systemPackages = with pkgs; [
-    protontricks
+    gamemode
+    osu-stable
+    wine-discord-ipc-bridge
   ];
+  services.pipewire.lowLatency.enable = true;
+  security.rtkit.enable = true; # make pipewire realtime-capable
 }
