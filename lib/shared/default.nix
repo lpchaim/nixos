90a2419e76cd808888d3ff9089fe426a6cf4a89d
Lucas Chaim <lpchaim@gmail.com>
2024-09-06 07:36:56 -0300

    chore: Update flake inputs, misc fixes

--- lib/shared/default.nix
+++ lib/shared/default.nix
@@ -13,12 +13,14 @@
       extra-experimental-features = "flakes nix-command";
       extra-substituters = lib.mkForce [];
       extra-trusted-substituters = [
+        "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://nyx.chaotic.cx"
         "https://snowflakeos.cachix.org"
       ];
       extra-trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
