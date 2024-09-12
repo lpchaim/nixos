3916cdbf5ce7a22d40c0840f634e3f61c2a4ab0d
Luna Perroni <lpchaim@proton.me>
2024-09-12 10:06:21 -0300

    refactor: Move repo init script to binary in deploy shell

--- shells/deploy/default.nix
+++ shells/deploy/default.nix
@@ -2,6 +2,7 @@
   lib,
   pkgs,
   mkShell,
+  writeShellScriptBin,
   ...
 }: let
   inherit (lib.lpchaim.shell) makeDevShellWithDefaults;
@@ -16,14 +17,19 @@ in
       helix
       home-manager
       nixos-rebuild
+      (writeShellScriptBin "flake-init" ''
+        if [ ! -d "$FLAKE" ]; then
+          mkdir -p "$FLAKE"
+          git clone --branch develop 'https://github.com/lpchaim/nixos' "$FLAKE"
+          echo "Repo checked out at $FLAKE"
+        else
+          echo 'Repo exists, nothing to do'
+        fi
+      '')
     ];
     shellHook = ''
       export NIX_CONFIG="${builtins.readFile pkgs.nix-conf}"
       export EDITOR=hx
       export FLAKE="$HOME/.config/nixos"
-
-      if [ ! -d "$FLAKE" ]; then
-        git clone "https://github.com/lpchaim/nixos" "$FLAKE"
-      fi
     '';
   }
