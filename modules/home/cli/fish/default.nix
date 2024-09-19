a7dce8cc08439f16b5239af9f012e42aa11e7c0f
Luna Perroni <lpchaim@proton.me>
2024-09-19 11:15:51 -0300

    feat(cli): Add fish plugins

--- modules/home/cli/fish/default.nix
+++ modules/home/cli/fish/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 lib.lpchaim.mkModule {
@@ -9,8 +10,20 @@ lib.lpchaim.mkModule {
   description = "fish shell";
   configBuilder = cfg: {
     programs.fish = {
-      enable = true;
       inherit (config.home) shellAliases;
+      enable = true;
+      plugins =
+        builtins.map
+        (name: {
+          inherit name;
+          inherit (pkgs.fishPlugins.${name}) src;
+        })
+        [
+          "fifc"
+          "foreign-env"
+          "pisces"
+          "puffer"
+        ];
     };
   };
 }
