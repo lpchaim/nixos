cacc233683bb3c86d0ce24ee86de2edb75f521f3
Lucas Chaim <lpchaim@gmail.com>
2024-09-27 13:05:44 -0300

    feat: Add my binary cache

--- lib/shared/default.nix
+++ lib/shared/default.nix
@@ -26,6 +26,7 @@ in {
       extra-substituters = extra-trusted-substituters;
       extra-trusted-substituters = [
         "https://hyprland.cachix.org"
+        "https://lpchaim.cachix.org"
         "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://nyx.chaotic.cx"
@@ -33,6 +34,7 @@ in {
       ];
       extra-trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "lpchaim.cachix.org-1:2xOuvojcUDNhJRzCpvgewQ2DdNZz3QzGVV4Z/7C+Lio="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
