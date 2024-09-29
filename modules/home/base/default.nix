06b2e33cd94578b93e53277a0b9de2241d0c2d85
Luna Perroni <lpchaim@proton.me>
2024-09-28 23:13:40 -0300

    feat: Add GitHub access token
    Also, it turns out home manager configurations do have access to the host
    configs when they aren't standalone. This is the first commit
    implementing that.

--- modules/home/base/default.nix
+++ modules/home/base/default.nix
@@ -2,7 +2,7 @@
   config,
   lib,
   ...
-}:
+} @ args:
 with lib; let
   inherit (lib.lpchaim) shared;
   inherit (lib.snowfall) fs;
@@ -31,6 +31,7 @@ in {
       options = "--delete-older-than=7d";
     };
     nix.settings = shared.nix.settings;
+    nix.extraOptions = mkIf (args ? osConfig) args.osConfig.nix.extraOptions;
     systemd.user.startServices = "sd-switch";
   };
 }
