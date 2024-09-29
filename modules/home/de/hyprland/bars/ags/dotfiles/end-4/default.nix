bb7c89b863ec7446213d4464f5eed0f48d77e146
Lucas Chaim <lpchaim@gmail.com>
2024-09-29 19:01:53 -0300

    fix: Remove obsolete packages
    Smartcat is already in nixpkgs from my own PR
    
    The materialyoucolor one was only used for a hyprland configurations I
    don't use anymore, so I've moved it into the specific corner it's used

--- modules/home/de/hyprland/bars/ags/dotfiles/end-4/default.nix
+++ modules/home/de/hyprland/bars/ags/dotfiles/end-4/default.nix
@@ -7,7 +7,23 @@
 }: let
   pythonWithMaterialYouColor = pkgs.python311.withPackages (p:
     with p; [
-      pkgs.lpchaim."pythonPackages.materialyoucolor"
+      (python3Packages.buildPythonPackage rec {
+        pname = "materialyoucolor";
+        version = "2.0.9";
+
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-J35//h3tWn20f5ej6OXaw4NKnxung9q7m0E4Zf9PUw4=";
+        };
+
+        doCheck = false;
+
+        meta = {
+          homepage = "https://github.com/T-Dynamos/materialyoucolor-python";
+          license = lib.licenses.mit;
+          maintainers = with lib.maintainers; [lpchaim];
+        };
+      })
       # material-color-utilities
       pywayland
       setproctitle
