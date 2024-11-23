18ff00c6e8b3582fa0ae9f9f5e7cad77ed68bc13
Lucas Chaim <lpchaim@gmail.com>
2024-11-23 18:34:41 -0300

    feat: Gaming tweaks, better proton-ge handling

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,13 @@
 {inputs, ...}: let
   inherit (inputs) self;
+  inherit (inputs.nixpkgs) lib;
 in
   final: prev: {
+    inherit
+      (inputs.chaotic.packages.${prev.system}.jovian-chaotic)
+      mesa-radeonsi-jupiter
+      mesa-radv-jupiter
+      ;
     nix-conf = let
       homeCfg = self.legacyPackages.${prev.system}.homeConfigurations.minimal.config.home;
       nixCfg = homeCfg.file."${homeCfg.homeDirectory}/.config/nix/nix.conf".source;
@@ -20,4 +26,25 @@ in
       ];
       mesonFlags = [];
     });
+    proton-ge-bin-versions = let
+      latestPackage = prev.proton-ge-bin;
+      legacyVersions = {
+        "7-55" = "sha256-6CL+9X4HBNoB/yUMIjA933XlSjE6eJC86RmwiJD6+Ws=";
+        "8-32" = "sha256-ZBOF1N434pBQ+dJmzfJO9RdxRndxorxbJBZEIifp0w4=";
+      };
+      legacyPackages =
+        lib.mapAttrs
+        (_version: hash:
+          latestPackage.overrideAttrs (_: rec {
+            pname = "proton-ge-bin-${_version}";
+            version = "GE-Proton${_version}";
+            src = prev.fetchzip {
+              url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
+              inherit hash;
+            };
+          }))
+        legacyVersions;
+    in
+      legacyPackages
+      // {"latest" = latestPackage;};
   }
