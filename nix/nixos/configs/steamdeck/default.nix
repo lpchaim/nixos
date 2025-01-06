ef20e40ec94ffc43093a203864abf4b262ece560
Lucas Chaim <lpchaim@proton.me>
2025-01-06 16:52:20 -0300

    feat: Dedicated steamos module

--- nix/nixos/configs/steamdeck/default.nix
+++ nix/nixos/configs/steamdeck/default.nix
@@ -1,58 +1,25 @@
 {
-  config,
   inputs,
-  lib,
+  ezModules,
   ...
 }: let
-  inherit (lib) mkIf mkForce;
   inherit (inputs.self.lib.config) name;
 in {
   imports = [
-    inputs.jovian.nixosModules.default
+    ezModules.steamos
     ./hardware-configuration.nix
     ./storage.nix
   ];
 
   my.profiles = {
-    kernel = false;
     de.gnome = true;
+    kernel = false;
   };
   my.gaming.enable = false;
   my.gaming.steam.enable = true;
+  my.modules.steamos.enable = true;
   my.security.u2f.relaxed = true;
 
-  jovian = {
-    steam = {
-      inherit (name) user;
-      enable = true;
-      autoStart = true;
-      desktopSession = "gnome";
-    };
-    steamos = {
-      enableMesaPatches = true;
-      useSteamOSConfig = true;
-    };
-    decky-loader.enable = true;
-    devices.steamdeck = {
-      enable = true;
-      autoUpdate = true;
-      enableGyroDsuService = true;
-    };
-  };
-
-  services = mkIf config.jovian.steam.autoStart {
-    displayManager.sddm.enable = mkForce false;
-    xserver.displayManager.gdm.enable = mkForce false;
-  };
-  time = {
-    hardwareClockInLocalTime = mkForce false;
-    timeZone = mkForce null;
-  };
-
   system.stateVersion = "24.05";
-
-  home-manager.users.${name.user} = {
-    dconf.settings."org/gnome/shell".favorite-apps = ["steam.desktop"];
-    home.stateVersion = "24.05";
-  };
+  home-manager.users.${name.user}.home.stateVersion = "24.05";
 }
