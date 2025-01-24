879e6c9bc28fc452b716f4863fc0309eb9c3e34e
Lucas Chaim <lpchaim@gmail.com>
2025-01-24 17:14:57 -0300

    fix: lastdl fixes and enhancements

--- nix/home/modules/scripts/lastdl.nix
+++ nix/home/modules/scripts/lastdl.nix
@@ -2,12 +2,18 @@
 pkgs.writers.writeNuBin "lastdl"
 # nu
 ''
+
   # Prints the last downloaded file
-  def main [ ]: nothing -> path {
+  def main [
+    --short # Prints the filename instead of full path
+    --quote # Prints the raw path with added quotes
+  ]: nothing -> string {
     ls ~/Downloads
     | where type == file
-    | sort-by modified --reverse
+    | sort-by modified
     | last
     | get name
+    | if $short { path basename } else { $in }
+    | if $quote { $'"($in)"' } else { $in }
   }
 ''
