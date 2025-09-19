fbab00f8bfb39c456db3cb7aea15c6405af61ba9
Luna Perroni <lpchaim@proton.me>
2025-09-19 15:37:04 -0300

    refactor: Move some security related packages to home configuration
    Allows use of common utilities on standalone home configurations

---
+++ nix/home/modules/security/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  osConfig ? {},
+  pkgs,
+  ...
+}: let
+  cfg = config.my.security;
+in {
+  options.my.security = {
+    enable =
+      lib.mkEnableOption "security settings"
+      // {default = osConfig.my.security.enable or true;};
+  };
+  config = lib.mkIf cfg.enable {
+    home.packages =
+      (with pkgs; [
+        yubikey-manager
+        yubikey-personalization
+      ])
+      ++ lib.optionals config.my.profiles.graphical [
+        pkgs.yubioath-flutter
+      ];
+  };
+}
