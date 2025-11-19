1f1ed36b2bf3b3b0798bcb30ae42fc6b09cd9eaf
Luna Perroni <lpchaim@proton.me>
2025-11-19 14:35:12 -0300

    feat: Add emily user

--- nix/nixos/modules/secrets/default.nix
+++ nix/nixos/modules/secrets/default.nix
@@ -8,8 +8,9 @@
     defaultSopsFile = inputs.self + /secrets/default.yaml;
     age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     secrets = {
-      password.neededForUsers = true;
       "tailscale/oauth/secret" = {};
+      "user/emily/password".neededForUsers = true;
+      "user/lpchaim/password".neededForUsers = true;
     };
   };
 }
