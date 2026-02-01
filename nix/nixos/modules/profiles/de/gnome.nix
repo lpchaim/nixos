de2be8ce148cb26d5dd489a338a6a8a7f8eef3bb
Luna Perroni <lpchaim@proton.me>
2026-02-01 11:56:42 -0300

    chore: Properly exclude most gnome apps

--- nix/nixos/modules/profiles/de/gnome.nix
+++ nix/nixos/modules/profiles/de/gnome.nix
@@ -13,42 +13,19 @@ in {
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
 
-    environment.gnome.excludePackages = let
-      getPackages = p: (builtins.filter (package: p ? package) (with p; [
-        aisleriot # patience game
-        atomix # puzzle game
-        cheese # webcam tool
-        # epiphany # web browser
-        # evince # document viewer
-        geary # email reader
-        gedit # text editor
-        gnome-initial-setup
-        gnome-music
-        gnome-terminal
-        hitori # sudoku game
-        iagno # go game
-        tali # poker game
-        totem # video player
-        five-or-more # game
-        four-in-a-row # game
-        gnome-chess # chess game
-        gnome-mines # mine game
-        gnome-software
-        gnome-sound-recorder
-        gnome-sudoku # sudoku game
-        gnome-system-monitor
-        gnome-taquin # game
-        gnome-tetravex # tetris game
-        hitori # game
-        quadrapassel # tetris game
-        sushi # nautilus previewer
-        swell-foop # puzzle game
-      ]));
-    in
-      (getPackages pkgs)
-      ++ (getPackages pkgs.gnome);
+    services.gnome = {
+      core-apps.enable = false;
+      games.enable = false;
+      gnome-keyring.enable = false;
+      core-developer-tools.enable = false;
+    };
+
+    environment.gnome.excludePackages = with pkgs; [
+      gnome-tour # GNOME Shell detects the .desktop file on first log-in.
+    ];
     environment.systemPackages = with pkgs; [
       gnome-photos
+      nautilus
     ];
   };
 }
