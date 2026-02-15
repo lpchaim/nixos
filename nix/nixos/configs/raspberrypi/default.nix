477b44dab0d0465fcde2eb0c473c57b76db41789
Luna Perroni <lpchaim@proton.me>
2026-02-15 13:57:37 -0300

    wip: Rekey all secrets with the correct master keys

--- nix/nixos/configs/raspberrypi/default.nix
+++ nix/nixos/configs/raspberrypi/default.nix
@@ -18,6 +18,7 @@ in {
   boot.kernelPackages = pkgs.linuxPackages_latest;
   nixpkgs.hostPlatform = "aarch64-linux";
 
+  age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILr9pl4qaL/+DV//lhE5y6V7xJ2eh1BSlwNYD9L9a2sQ";
   system.stateVersion = "24.05";
   home-manager.users.${name.user}.home.stateVersion = "24.05";
 }
