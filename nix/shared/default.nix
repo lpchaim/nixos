9ae3e007306133368e47eb6022981f998058a099
Luna Perroni <lpchaim@proton.me>
2026-02-17 20:02:51 -0300

    chore: Improve shared nix options

--- nix/shared/default.nix
+++ nix/shared/default.nix
@@ -4,18 +4,14 @@
   ...
 }: let
   inherit (inputs) self;
-  inherit (self.lib.config.nix) settings;
 in {
   imports = [
+    ./nix.nix
     ./secrets.nix
     ./theming.nix
   ];
 
   options = {
     my.config = lib.mkOption {default = self.lib.config;};
-  };
-
-  config = {
-    nix = {inherit settings;};
   };
 }
