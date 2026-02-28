020f617c6ed710d372a9fc90e33720d8fc3b2b4b
Luna Perroni <lpchaim@proton.me>
2026-02-27 22:31:15 -0300

    feat(syncthing): Migrate syncthing to home configuration

--- nix/home/modules/syncthing/default.nix
+++ nix/home/modules/syncthing/default.nix
@@ -2,124 +2,121 @@
   config,
   lib,
   pkgs,
+  self,
   osConfig ? {},
   ...
 }: let
-  syncthing = osConfig.services.syncthing.package or config.services.syncthing.package;
+  inherit (self.lib.secrets.helpers) mkHostSecret;
   syncthingtray = config.services.syncthing.tray.package;
   cfg = config.my.syncthing;
 in {
-  options.my.syncthing.enable =
-    lib.mkEnableOption "syncthing"
-    // {default = osConfig.my.syncthing.enable or false;};
+  options.my.syncthing = {
+    enable =
+      lib.mkEnableOption "syncthing"
+      // {default = osConfig.my.syncthing.enable or false;};
+    host = lib.mkOption {
+      type = with lib.types; nullOr str;
+      default = osConfig.networking.hostName or null;
+    };
+  };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [syncthingtray];
+    assertions = [
+      {
+        assertion = cfg.host != null;
+        message = "config.my.syncthing.host must be set";
+      }
+    ];
+
+    my.secretDefinitions = let
+      owner =
+        if (osConfig != {})
+        then config.home.username
+        else "0";
+    in
+      lib.mkIf (cfg.host != null) {
+        "host.syncthing-cert" = mkHostSecret cfg.host "syncthing-cert" {
+          inherit owner;
+        };
+        "host.syncthing-key" = mkHostSecret cfg.host "syncthing-key" {
+          inherit owner;
+        };
+      };
+
     services.syncthing = {
-      enable = osConfig == {};
+      enable = true;
       tray.enable = true;
+      cert = config.my.secrets."host.syncthing-cert".path;
+      key = config.my.secrets."host.syncthing-key".path;
+      settings = {
+        gui.theme = "dark";
+        options = {
+          localAnnounceEnabled = true;
+          relaysEnabled = true;
+          urAccepted = -1; # For no and don't ask again
+        };
+        startBrowser = false;
+        folders = let
+          computers = ["desktop" "laptop" "steamdeck"];
+          phones = ["galaxyS23"];
+          servers = ["server"];
+          allDevices = computers ++ phones ++ servers;
+          trashVersioning = {
+            type = "trashcan";
+            params.cleanoutDays = "30";
+          };
+        in {
+          "~/Sync" = {
+            id = "default";
+            label = "Default Folder";
+            type = "sendreceive";
+            versioning = null;
+            devices = allDevices;
+          };
+          "~/Notes/Logseq" = {
+            id = "6ymhp-fehcm";
+            label = "Notes/Logseq";
+            type = "sendreceive";
+            versioning = trashVersioning;
+            devices = allDevices;
+          };
+          "~/Notes/Obsidian" = {
+            id = "tgnpg-efws9";
+            label = "Notes/Obsidian";
+            type = "sendreceive";
+            versioning = trashVersioning;
+            devices = allDevices;
+          };
+          "~/.steam/steam/userdata/85204334/config/grid" = {
+            id = "steam-custom-icons";
+            label = "Steam/Custom Icons";
+            type = "sendreceive";
+            versioning = null;
+            devices = computers ++ servers;
+          };
+        };
+        devices = {
+          desktop.id = "Q7UXFUW-Q4QWALL-AVBRBPW-Y2S44CV-IR4H3V4-OT2GH4V-6WCXBR4-STJXFQJ";
+          desktop.name = "Desktop";
+          laptop.id = "VFFQPOF-XAPVKHO-4PUSIVT-ACYNHAZ-GOQBWC6-SEYBXGE-2MBBMRS-TJRD4QL";
+          laptop.name = "Laptop";
+          pixel7.id = "PDMAJC4-SIXM4NI-UDMSLPU-3QSBSM2-ZUBLQDU-MNCR2HH-XUJIG52-PH4IKQC";
+          pixel7.name = "Pixel 7 Pro";
+          galaxyS23.id = "DPARDTW-7LHI6VK-CRKEYI4-VK6BWWP-DMW6KOG-6LWAT4O-QFGDFPR-XVO6RAF";
+          galaxyS23.name = "Galaxy S23";
+          server.id = "X5LHXQ6-NOCD2NO-RQ7FPLO-WFLLFRE-5BTTVL6-XLH3DAV-4ZIYI47-EEOVYAK";
+          server.name = "Server";
+          steamdeck.id = "OBZRWRW-B7DYVZC-RL5JV3D-6YNWG4O-MAIN2GY-KTEBY6V-DWQK36S-5E2O7AB";
+          steamdeck.name = "Steam Deck";
+        };
+      };
     };
+
+    home.packages = [syncthingtray];
     systemd.user.services.syncthingtray = {
       Service.ExecStart = lib.mkForce (pkgs.writeShellScript "syncthingtray-wait" ''
         ${syncthingtray}/bin/syncthingtray --wait
       '');
-      Service.ExecStartPre = pkgs.writeShellScript "setup-syncthingtray" ''
-          cat <<EOF >> ~/.config/syncthingtray.ini
-          [General]
-          v=${syncthingtray.version}
-
-          [startup]
-          considerForReconnect=false
-          considerLauncherForReconnect=false
-          showButton=false
-          showLauncherButton=false
-          stopOnMetered=false
-          stopServiceOnMetered=false
-          syncthingArgs="serve --no-browser --logflags=3"
-          syncthingAutostart=false
-          syncthingPath=syncthing
-          syncthingUnit=syncthing.service
-          systemUnit=false
-          useLibSyncthing=false
-
-          [tray]
-          connections\1\apiKey=@ByteArray($(${syncthing}/bin/syncthing cli config dump-json | nu --stdin --commands 'from json | get gui.apiKey'))
-          connections\1\authEnabled=false
-          connections\1\autoConnect=true
-          connections\1\devStatsPollInterval=60000
-          connections\1\diskEventLimit=200
-          connections\1\errorsPollInterval=30000
-          connections\1\httpsCertPath=/home/lpchaim/.config/syncthing/https-cert.pem
-          connections\1\label=Primary instance
-          connections\1\longPollingTimeout=0
-          connections\1\password=
-          connections\1\pauseOnMetered=false
-          connections\1\reconnectInterval=30000
-          connections\1\requestTimeout=0
-          connections\1\statusComputionFlags=59
-          connections\1\syncthingUrl=http://127.0.0.1:8384
-          connections\1\trafficPollInterval=5000
-          connections\1\userName=
-          connections\size=1
-          dbusNotifications=true
-          distinguishTrayIcons=false
-          frameStyle=16
-          ignoreInavailabilityAfterStart=15
-          notifyOnDisconnect=true
-          notifyOnErrors=true
-          notifyOnLauncherErrors=true
-          notifyOnLocalSyncComplete=false
-          notifyOnNewDeviceConnects=false
-          notifyOnNewDirectoryShared=false
-          notifyOnRemoteSyncComplete=false
-          positioning\assumedIconPos=@Point(0 0)
-          positioning\useAssumedIconPosition=false
-          positioning\useCursorPos=true
-          preferIconsFromTheme=false
-          showSyncthingNotifications=true
-          showTabTexts=true
-          showTraffic=true
-          statusIcons="#ff26b6db,#ff0882c8,#ffffffff;#ffdb3c26,#ffc80828,#ffffffff;#ffc9ce3b,#ffebb83b,#ffffffff;#ff2d9d69,#ff2d9d69,#ffffffff;#ff26b6db,#ff0882c8,#ffffffff;#ff26b6db,#ff0882c8,#ffffffff;#ffa9a9a9,#ff58656c,#ffffffff;#ffa9a9a9,#ff58656c,#ffffffff"
-          statusIconsRenderSize=@Size(32 32)
-          statusIconsStrokeWidth=0
-          tabPos=1
-          trayIcons="#ff26b6db,#ff0882c8,#ffffffff;#ffdb3c26,#ffc80828,#ffffffff;#ffc9ce3b,#ffebb83b,#ffffffff;#ff2d9d69,#ff2d9d69,#ffffffff;#ff26b6db,#ff0882c8,#ffffffff;#ff26b6db,#ff0882c8,#ffffffff;#ffa9a9a9,#ff58656c,#ffffffff;#ffa9a9a9,#ff58656c,#ffffffff"
-          trayIconsRenderSize=@Size(32 32)
-          trayIconsStrokeWidth=0
-          trayMenuSize=@Size(575 475)
-          usePaletteForStatusIcons=false
-          usePaletteForTrayIcons=false
-          windowType=0
-
-          [webview]
-          customCommand=
-          disabled=false
-          mode=0
-          qt\customfont=false
-          qt\customicontheme=false
-          qt\customlocale=false
-          qt\custompalette=false
-          qt\customstylesheet=false
-          qt\customwidgetstyle=false
-          qt\font="Sans Serif,9,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"
-          qt\icontheme=hicolor
-          qt\iconthemepath=
-          qt\locale=en_US
-          qt\palette=@Variant(\0\0\0\x44\x1\x1\xff\xff\0\0\0\0\0\0\0\0\x1\x1\xff\xff\xef\xef\xef\xef\xef\xef\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\xcb\x4\xcb\x4\xcb\x4\0\0\x1\x1\xff\xff\x9f\xf4\x9f\xf4\x9f\xf4\0\0\x1\x1\xff\xff\xb8\x90\xb8\x90\xb8\x90\0\0\x1\x1\xff\xff\0\0\0\0\0\0\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\0\0\0\0\0\0\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\xef\xef\xef\xef\xef\xef\0\0\x1\x1\xff\xffv{v{v{\0\0\x1\x1\xff\xff\x30\x30\x8c\x8c\xc6\xc6\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\0\0\0\0\xff\xff\0\0\x1\x1\xff\xff\xff\xff\0\0\xff\xff\0\0\x1\x1\xff\xff\xf7\xf7\xf7\xf7\xf7\xf7\0\0\x1\x1\xff\xff\xbe\xbe\xbe\xbe\xbe\xbe\0\0\x1\x1\xff\xff\xef\xef\xef\xef\xef\xef\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\xcb\x4\xcb\x4\xcb\x4\0\0\x1\x1\xff\xff\xbe\xbe\xbe\xbe\xbe\xbe\0\0\x1\x1\xff\xff\xb8\x90\xb8\x90\xb8\x90\0\0\x1\x1\xff\xff\xbe\xbe\xbe\xbe\xbe\xbe\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\xbe\xbe\xbe\xbe\xbe\xbe\0\0\x1\x1\xff\xff\xef\xef\xef\xef\xef\xef\0\0\x1\x1\xff\xff\xef\xef\xef\xef\xef\xef\0\0\x1\x1\xff\xff\xb1\xb8\xb1\xb8\xb1\xb8\0\0\x1\x1\xff\xff\x91\x91\x91\x91\x91\x91\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\0\0\0\0\xff\xff\0\0\x1\x1\xff\xff\xff\xff\0\0\xff\xff\0\0\x1\x1\xff\xff\xf7\xf7\xf7\xf7\xf7\xf7\0\0\x1\x1\xff\xff\0\0\0\0\0\0\0\0\x1\x1\xff\xff\xef\xef\xef\xef\xef\xef\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\xcb\x4\xcb\x4\xcb\x4\0\0\x1\x1\xff\xff\x9f\xf4\x9f\xf4\x9f\xf4\0\0\x1\x1\xff\xff\xb8\x90\xb8\x90\xb8\x90\0\0\x1\x1\xff\xff\0\0\0\0\0\0\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\0\0\0\0\0\0\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\xef\xef\xef\xef\xef\xef\0\0\x1\x1\xff\xffv{v{v{\0\0\x1\x1\xff\xff\x30\x30\x8c\x8c\xc6\xc6\0\0\x1\x1\xff\xff\xff\xff\xff\xff\xff\xff\0\0\x1\x1\xff\xff\0\0\0\0\xff\xff\0\0\x1\x1\xff\xff\xff\xff\0\0\xff\xff\0\0\x1\x1\xff\xff\xf7\xf7\xf7\xf7\xf7\xf7\0\0)
-          qt\plugindir=
-          qt\stylesheetpath=
-          qt\trpath=
-          qt\widgetstyle=
-        EOF
-      '';
-      Unit.X-Restart-Triggers = [
-        syncthing
-        syncthingtray
-        "syncthing.systemunit.service"
-        "syncthing-init.systemunit.service"
-        "syncthing-relay.systemunit.service"
-      ];
-      Unit.X-SwitchMethod = "restart";
     };
   };
 }
--- nix/nixos/modules/syncthing/default.nix
+++ nix/nixos/modules/syncthing/default.nix
@@ -1,102 +1,16 @@
 {
   config,
-  inputs,
   lib,
   ...
 }: let
-  inherit (config.my.config) name;
-  inherit (inputs.self.lib.secrets.helpers) mkHostSecret;
   cfg = config.my.syncthing;
-  home = config.home-manager.users.lpchaim.home.homeDirectory;
 in {
   options.my.syncthing.enable = lib.mkEnableOption "syncthing";
 
   config = lib.mkIf cfg.enable {
-    my.secretDefinitions = {
-      "host.syncthing-cert" = mkHostSecret config "syncthing-cert" {
-        mode = "0440";
-      };
-      "host.syncthing-key" = mkHostSecret config "syncthing-key" {
-        mode = "0440";
-      };
-    };
-
-    systemd.services.syncthing.preStart = let
-      paths = builtins.attrNames config.services.syncthing.settings.folders;
-      commands = map (p: "mkdir -p '${p}'") paths;
-      script = builtins.concatStringsSep "\n" commands;
-    in
-      script;
-
-    services.syncthing = {
-      enable = true;
-      relay.enable = true;
-      openDefaultPorts = true;
-      user = name.user;
-      group = name.user;
-      cert = config.my.secrets."host.syncthing-cert".path;
-      key = config.my.secrets."host.syncthing-key".path;
-      dataDir = "${home}/Syncthing";
-      configDir = "${home}/.config/syncthing";
-      settings = {
-        gui.theme = "black";
-        options.relaysEnabled = true;
-        options.urAccepted = -1; # For no and don't ask again
-        options.localAnnounceEnabled = true;
-        startBrowser = false;
-        folders = let
-          computers = ["desktop" "laptop" "steamdeck" "server"];
-          phones = ["pixel7" "galaxyS23"];
-          allDevices = computers ++ phones;
-          trashVersioning = {
-            type = "trashcan";
-            params.cleanoutDays = "30";
-          };
-        in {
-          "${home}/Sync" = {
-            id = "default";
-            label = "Default Folder";
-            type = "sendreceive";
-            versioning = null;
-            devices = allDevices;
-          };
-          "${home}/Notes/Logseq" = {
-            id = "6ymhp-fehcm";
-            label = "Notes/Logseq";
-            type = "sendreceive";
-            versioning = trashVersioning;
-            devices = allDevices;
-          };
-          "${home}/Notes/Obsidian" = {
-            id = "tgnpg-efws9";
-            label = "Obsidian";
-            type = "sendreceive";
-            versioning = trashVersioning;
-            devices = allDevices;
-          };
-          "${home}/.steam/steam/userdata/85204334/config/grid" = {
-            id = "steam-custom-icons";
-            label = "Steam/Custom Icons";
-            type = "sendreceive";
-            versioning = null;
-            devices = computers;
-          };
-        };
-        devices = {
-          desktop.id = "Q7UXFUW-Q4QWALL-AVBRBPW-Y2S44CV-IR4H3V4-OT2GH4V-6WCXBR4-STJXFQJ";
-          desktop.name = "Desktop";
-          laptop.id = "VFFQPOF-XAPVKHO-4PUSIVT-ACYNHAZ-GOQBWC6-SEYBXGE-2MBBMRS-TJRD4QL";
-          laptop.name = "Laptop";
-          pixel7.id = "PDMAJC4-SIXM4NI-UDMSLPU-3QSBSM2-ZUBLQDU-MNCR2HH-XUJIG52-PH4IKQC";
-          pixel7.name = "Pixel 7 Pro";
-          galaxyS23.id = "DPARDTW-7LHI6VK-CRKEYI4-VK6BWWP-DMW6KOG-6LWAT4O-QFGDFPR-XVO6RAF";
-          galaxyS23.name = "Galaxy S23";
-          server.id = "X5LHXQ6-NOCD2NO-RQ7FPLO-WFLLFRE-5BTTVL6-XLH3DAV-4ZIYI47-EEOVYAK";
-          server.name = "Server";
-          steamdeck.id = "OBZRWRW-B7DYVZC-RL5JV3D-6YNWG4O-MAIN2GY-KTEBY6V-DWQK36S-5E2O7AB";
-          steamdeck.name = "Steam Deck";
-        };
-      };
+    networking.firewall = {
+      allowedTCPPorts = [22000];
+      allowedUDPPorts = [21027 22000];
     };
   };
 }
