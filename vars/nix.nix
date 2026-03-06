8221341a850e231bb208b724e527da388c86064d
Luna Perroni <lpchaim@proton.me>
2026-03-06 17:52:21 -0300

    chore: Reorder nix settings alphabetically

--- vars/nix.nix
+++ vars/nix.nix
@@ -5,9 +5,14 @@
   };
   settings = {
     accept-flake-config = true;
-    builders-use-substitutes = true;
     auto-optimise-store = true;
+    builders-use-substitutes = true;
     extra-experimental-features = "flakes nix-command pipe-operator";
+    http-connections = 100;
+    keep-derivations = true;
+    keep-outputs = true;
+    max-jobs = "auto";
+    max-substitution-jobs = 100;
     substituters = [
       "https://cache.nixos.org?priority=1"
       "https://nix-community.cachix.org?priority=2"
@@ -20,11 +25,6 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
-    http-connections = 100;
-    keep-derivations = true;
-    keep-outputs = true;
-    max-jobs = "auto";
-    max-substitution-jobs = 100;
     trusted-users = ["root" "@wheel"];
   };
 }
